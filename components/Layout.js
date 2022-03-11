9627a088031000fb8c2fafbd5d027ed43901644c
joao <veigajoaoa@gmail.com>
2022-03-11 16:13:47 -0300

    new version

--- components/Layout.js
+++ components/Layout.js
@@ -196,19 +196,19 @@ export default class Layout extends Component {
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
                     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Source+Sans+Pro:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet"/>
 
-                    <script defer src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-                    <script defer src="/assets/vendor/aos/aos.js"></script>
-                    <script defer src="/assets/vendor/glightbox/js/glightbox.min.js"></script>
-                    <script defer src="/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-                    <script defer src="/assets/vendor/swiper/swiper-bundle.min.js"></script>
-                    <script defer src="/assets/vendor/swiper/swiper-bundle.min.js"></script>
-                    <script defer src="/assets/js/main.js"></script>
+                    <script defer src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+                    <script defer src="assets/vendor/aos/aos.js"></script>
+                    <script defer src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+                    <script defer src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+                    <script defer src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+                    <script defer src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+                    <script defer src="assets/js/main.js"></script>
 
                 </Head>
                 <header id="header" className="header fixed-top" data-scrollto-offset="0">
                     <div className="container-fluid d-flex align-items-center justify-content-between">
                         <a href="index.html" className="logo d-flex align-items-center ms-3 me-auto me-lg-0">
-                            <img src="/assets/img/logo.png" alt=""/>
+                            <img src="assets/img/logo.png" alt=""/>
                         </a>
     
                         {metaMaskButton1}
--- components/WhitelistHero.js
+++ components/WhitelistHero.js
@@ -6,7 +6,7 @@ export default props => {
     return (
         <section id="hero-animated" className="hero-animated d-flex align-items-center bg-secondary">
             <div className="container d-flex flex-column justify-content-center align-items-center text-center position-relative" data-aos="zoom-out">
-            <img src="/assets/img/whitelist/Case Gold Bananas.png" className="img-fluid animated" style={{height: 50 + 'vh', marginBottom: 5 + 'vh'}}/>
+            <img src="assets/img/whitelist/Case Gold Bananas.png" className="img-fluid animated" style={{height: 50 + 'vh', marginBottom: 5 + 'vh'}}/>
             
             <div className="d-block">
                 <h2>TIME TO HODL <span>$BNANA</span></h2>
