a4e486df72d98773cf1b221b311ea82744a6c9fa
joao <veigajoaoa@gmail.com>
2022-03-14 00:50:41 -0300

    add gtm

--- components/Layout.js
+++ components/Layout.js
@@ -202,6 +202,12 @@ export default class Layout extends Component {
                     <meta content="" name="description"/>
                     <meta content="" name="keywords"/>
 
+                    <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-KB7PSPQ');`}}></script>
+
                     <link href="assets/img/favicon.png" rel="icon"/>
                     <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon"/>
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
